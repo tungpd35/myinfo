dc09f867b116e9e4eb2c94c461464348a97759e9
tungpd <tungpd35@gmail.com>
2024-02-12 20:23:01 +0700

    commit

---
+++ src/main/java/com/example/myinfo/MyInfoApplication.java
@@ -0,0 +1,13 @@
+package com.example.myinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyInfoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyInfoApplication.class, args);
+    }
+
+}
